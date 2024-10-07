8f9b0e5a84de7d224295360a10c0b83f6bee91d3
ManjunathGoudar404 <manjunathgoudar404@gmail.com>
2024-10-07 22:06:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple", {
+    strings: ["Front End Developer", "Back End Developer", "Web Developer", "Database Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
