eadaf551cc623934220682cd4fed4cdbe8d46698
ManjunathGoudar404 <manjunathgoudar404@gmail.com>
2024-10-08 12:01:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ var typed = new Typed(".multiple", {
     backDelay: 1000,
     loop: true
 });
+function textToAudio() {
+    let speech = new SpeechSynthesisUtterance();
+    
+    speech.lang = "en-US";
+    speech.text = "Welcome to the Home Page! You can explore it by clicking the sidebar options or interacting with the moving elements.";
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    speechSynthesis.speak(speech);
+}
+// setInterval(textToAudio, 20000);
+// window.addEventListener('DOMContentLoaded', function() {
+//     textToAudio();
+// });
